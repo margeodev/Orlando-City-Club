808ed46050d9ae37a0ea5d8d7ad814bcca405bc5
margeodev <margeodev@gmail.com>
2016-12-23 13:02:04 -0300

    First commit, index html file created and add bootstrap and jquery library

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById("btn-calcular").onclick=function(){
+    var a = document.getElementById("valor-a").value;
+    var a = document.getElementById("valor-a").value;    
+    alert(a+b);
+}
